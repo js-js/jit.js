341ddb0fa130c8e3776eacf698aab078eafd9584
Fedor Indutny <fedor.indutny@gmail.com>
2013-03-09 00:11:26 +0400

    x64: fix high registers usage

--- test/api-test.js
+++ test/api-test.js
@@ -9,6 +9,7 @@ describe('JIT.js', function() {
         this.movq('rsp', 'rbp');
         this.movq(42, 'rax');
         this.movq('rbp', 'rsp');
+        this.nop();
         this.pop('rbp');;
         this.ret();
       } else if (this.arch === 'ia32') {
@@ -16,6 +17,7 @@ describe('JIT.js', function() {
         this.movl('esp', 'ebp');
         this.movl(42, 'eax');
         this.movl('ebp', 'esp');
+        this.nop();
         this.pop('ebp');;
         this.ret();
       }
@@ -23,4 +25,22 @@ describe('JIT.js', function() {
 
     assert.equal(fn(), 42);
   });
+
+  if (process.arch === 'x64') {
+    it('should compile function with high registers', function() {
+      var fn = jit.compile(function() {
+        this.push('rbp');
+        this.movq('rsp', 'rbp');
+        this.push('r11');
+        this.movq(42, 'r11');
+        this.movq('r11', 'rax');
+        this.pop('r11');
+        this.movq('rbp', 'rsp');
+        this.pop('rbp');;
+        this.ret();
+      });
+
+      assert.equal(fn(), 42);
+    });
+  }
 });
