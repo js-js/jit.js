466fba223a50703ff64b0e953fedfc4fa034992f
Fedor Indutny <fedor.indutny@gmail.com>
2013-03-09 14:49:54 +0400

    api: .getBackend()
    test: split tests

--- test/generic-test.js
+++ test/generic-test.js
@@ -1,7 +1,7 @@
 var jit = require('..'),
     assert = require('assert');
 
-describe('JIT.js', function() {
+describe('JIT.js generic', function() {
   it('should compile basic function', function() {
     var fn = jit.compile(function() {
       if (this.arch === 'x64') {
@@ -25,22 +25,4 @@ describe('JIT.js', function() {
 
     assert.equal(fn(), 42);
   });
-
-  if (process.arch === 'x64') {
-    it('should compile function with high registers', function() {
-      var fn = jit.compile(function() {
-        this.push('rbp');
-        this.movq('rsp', 'rbp');
-        this.push('r11');
-        this.movq(42, 'r11');
-        this.movq('r11', 'rax');
-        this.pop('r11');
-        this.movq('rbp', 'rsp');
-        this.pop('rbp');;
-        this.ret();
-      });
-
-      assert.equal(fn(), 42);
-    });
-  }
 });
