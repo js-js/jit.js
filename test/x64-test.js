21ec8292464a8fd07690f14a2a192d76c42cd6bc
Fedor Indutny <fedor.indutny@gmail.com>
2013-10-31 16:06:04 +0400

    math: more instructions, test

--- test/x64-test.js
+++ test/x64-test.js
@@ -3,24 +3,51 @@ var jit = require('..'),
 
 if (process.arch !== 'x64') return;
 
-describe('JIT.js x64', function() {
-  it('should compile function with high registers', function() {
-    var fn = jit.compile(function() {
-      this.Entry();
-
-      this.xor('r11', 'r11');
-      this.push('r11');
-      this.mov('r11', 34);
-      this.mov('rax', 'r11');
-      this.pop('r11');
-      this.add('r11', 13);
-      this.sub('rax', 'r11');
-      this.add('rax', 'rax');
-
-      this.Exit();
-    });
-
-    assert.equal(fn(), 42);
+function test(name, fn, result) {
+  it(name, function() {
+    assert.equal(jit.compile(fn)(), result);
   });
+}
 
+describe('JIT.js x64', function() {
+  test('should compile function with high registers', function() {
+    this.Entry();
+
+    this.xor('r11', 'r11');
+    this.push('r11');
+    this.mov('r11', 34);
+    this.mov('rax', 'r11');
+    this.pop('r11');
+    this.add('r11', 13);
+    this.sub('rax', 'r11');
+    this.add('rax', 'rax');
+
+    this.Exit();
+  }, 42);
+
+  test('should support math basics', function() {
+    this.Entry();
+
+    // 12 * 14
+    this.mov('r8', 12);
+    this.mov('rax', 14);
+    this.imul('r8');
+
+    // += 5
+    this.mov('r8', 'rax');
+    this.mov('rbx', 5);
+    this.add('rbx', 'r8');
+
+    // -= 3
+    this.mov('r8', 'rbx');
+    this.mov('rbx', new Buffer('0000000000000003', 'hex'));
+    this.sub('r8', 'rbx');
+
+    // /= 5
+    this.mov('rax', 'r8');
+    this.mov('r8', 5);
+    this.idiv('r8');
+
+    this.Exit();
+  }, 34);
 });
