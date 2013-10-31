7a984f7a8979f42a05198bc14d3590a0da274796
Fedor Indutny <fedor.indutny@gmail.com>
2013-10-31 15:11:18 +0400

    x64: stub out basics
    * temporarly remove ia32

--- test/x64-test.js
+++ test/x64-test.js
@@ -7,13 +7,20 @@ describe('JIT.js x64', function() {
   it('should compile function with high registers', function() {
     var fn = jit.compile(function() {
       this.Entry();
+
+      this.xor('r11', 'r11');
       this.push('r11');
-      this.movq(42, 'r11');
-      this.movq('r11', 'rax');
+      this.mov('r11', 34);
+      this.mov('rax', 'r11');
       this.pop('r11');
+      this.add('r11', 13);
+      this.sub('rax', 'r11');
+      this.add('rax', 'rax');
+
       this.Exit();
     });
 
     assert.equal(fn(), 42);
   });
+
 });
