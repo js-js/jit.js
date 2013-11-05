38a826ee2d0fe4ed1a62f80a361d417e23e2827c
Fedor Indutny <fedor.indutny@gmail.com>
2013-11-05 23:59:21 +0400

    example: more comments

--- example/heap/stubs.js
+++ example/heap/stubs.js
@@ -3,7 +3,7 @@ var Buffer = require('buffer').Buffer;
 
 var stubs = jit.stubs();
 
-// Fixed size memory chunk
+// Create fixed size memory chunk
 var page = new Buffer(1024);
 
 // Set-up pointers to page start and page end
@@ -14,27 +14,38 @@ stubs.define('Alloc', function() {
 
   // Save 'rbx' and 'rbx' registers
   this.spill(['rbx', 'rcx'], function() {
-    // Load offset
+    // Load `offset`
+    //
+    // NOTE: We'll use pointer to `offset` variable, to be able to update
+    // it below
     this.mov('rax', this.ptr(offset));
     this.mov('rax', ['rax']);
 
     // Load end
+    //
+    // NOTE: Same applies to end, though, we're not updating it right now
     this.mov('rbx', this.ptr(end));
     this.mov('rbx', ['rbx']);
 
-    // Calculate new offset
+    // Calculate new `offset`
     this.mov('rcx', 'rax');
+
+    // We'll assume that all allocations are 16 bytes = two 64bit pointers
     this.add('rcx', 16);
 
-    // Check if we won't overflow
+    // Check if we won't overflow our fixed size buffer
     this.cmp('rcx', 'rbx');
+
+    // this.j() performs conditional jump to the specified label.
+    // 'g' stands for 'greater'
+    // 'overflow' is a label name, bound below
     this.j('g', 'overflow');
 
     // Ok, we're good to go, update offset
     this.mov('rbx', this.ptr(offset));
     this.mov(['rbx'], 'rcx');
 
-    // Set tag
+    // First 64bit pointer is reserved for 'tag', second one is a `double` value
     this.mov(['rax'], 1);
   });
 
@@ -43,6 +54,10 @@ stubs.define('Alloc', function() {
 
   // Overflowed :(
   this.bind('overflow')
+
+  // Invoke javascript function!
+  // NOTE: This is really funky stuff, but I'm not going to dive deep
+  // into it right now
   this.runtime(function() {
     console.log('GC is needed, but not implemented');
   });
