38d4d3658cfedb40d934967d533b501251b9cff7
Fedor Indutny <fedor.indutny@gmail.com>
2014-01-28 15:59:01 +0400

    lib: initial pass on minimizing binding

--- src/jit.cc
+++ src/jit.cc
@@ -1,7 +1,6 @@
 #include "jit.h"
 #include "node_buffer.h"
 
-#include <sys/mman.h>
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
@@ -12,7 +11,6 @@ using namespace v8;
 using namespace node;
 
 static Persistent<String> sym_buffer;
-static size_t kPageSize;
 
 typedef intptr_t (*JitFnArg0)(void);
 typedef intptr_t (*JitFnArg1)(intptr_t);
@@ -20,38 +18,12 @@ typedef intptr_t (*JitFnArg2)(intptr_t, intptr_t);
 typedef intptr_t (*JitFnArg3)(intptr_t, intptr_t, intptr_t);
 
 
-static void DeallocateNop(char* data, void* hint) {
-  // Do nothing
-}
-
-
-inline size_t RoundUp(size_t a, size_t b) {
-  size_t mod = a % b;
-
-  return mod == 0 ? a == 0 ? b : a : a + (b - mod);
-}
-
-
 inline Buffer* GetPointerBuffer(void* ptr) {
   return Buffer::New(reinterpret_cast<char*>(&ptr), sizeof(&ptr));
 }
 
 
-ExecInfo::ExecInfo(void* exec, size_t elen, void* guard, size_t glen)
-      : exec_(exec),
-        guard_(guard),
-        elen_(elen),
-        glen_(glen) {
-}
-
-
-ExecInfo::~ExecInfo() {
-  munmap(exec_, elen_);
-  munmap(guard_, glen_);
-}
-
-
-Handle<Value> ExecInfo::New(const Arguments& args) {
+Handle<Value> FunctionWrap::New(const Arguments& args) {
   HandleScope scope;
 
   if (args.Length() < 1 ||
@@ -61,72 +33,36 @@ Handle<Value> ExecInfo::New(const Arguments& args) {
         "First argument should be Buffer!")));
   }
 
-  char* data = Buffer::Data(args[0].As<Object>());
-  size_t len = Buffer::Length(args[0].As<Object>());
+  char* exec = Buffer::Data(args[0].As<Object>());
 
-  size_t elen = RoundUp(len, kPageSize);
-  void* exec = mmap(NULL,
-                    elen,
-                    PROT_READ | PROT_WRITE | PROT_EXEC,
-                    MAP_ANON | MAP_PRIVATE,
-                    -1,
-                    0);
-  if (exec == MAP_FAILED) {
-    return ThrowException(Exception::TypeError(String::New(
-        "Failed to mmap executable page")));
-  }
+  FunctionWrap* wrap = new FunctionWrap(exec);
+  wrap->Wrap(args.This());
 
-  size_t glen = kPageSize;
-  void* guard = mmap(reinterpret_cast<char*>(exec) + elen,
-                     glen,
-                     PROT_NONE,
-                     MAP_ANON | MAP_PRIVATE,
-                     -1,
-                     0);
-  if (guard == MAP_FAILED) {
-    munmap(exec, elen);
-    return ThrowException(Exception::TypeError(String::New(
-        "Failed to mmap guard page")));
-  }
-
-  // Copy code into it
-  memcpy(exec, data, len);
-
-  // Fill rest with 0xcc
-  memset(reinterpret_cast<char*>(exec) + len, 0xcc, elen - len);
-
-  ExecInfo* info = new ExecInfo(exec, elen, guard, glen);
-  info->Wrap(args.This());
-
-  Buffer* buf = Buffer::New(reinterpret_cast<char*>(exec),
-                            elen,
-                            DeallocateNop,
-                            info);
-  args.This()->Set(sym_buffer, buf->handle_, ReadOnly);
+  args.This()->Set(sym_buffer, args[0], ReadOnly);
 
   return scope.Close(args.This());
 }
 
 
-Handle<Value> ExecInfo::Exec(const Arguments& args) {
+Handle<Value> FunctionWrap::Exec(const Arguments& args) {
   HandleScope scope;
 
-  ExecInfo* info = ObjectWrap::Unwrap<ExecInfo>(args.This());
+  FunctionWrap* wrap = ObjectWrap::Unwrap<FunctionWrap>(args.This());
 
   intptr_t ret;
   switch (args.Length()) {
    case 0:
-    ret = reinterpret_cast<JitFnArg0>(info->exec_)();
+    ret = reinterpret_cast<JitFnArg0>(wrap->exec_)();
     break;
    case 1:
-    ret = reinterpret_cast<JitFnArg1>(info->exec_)(args[0]->IntegerValue());
+    ret = reinterpret_cast<JitFnArg1>(wrap->exec_)(args[0]->IntegerValue());
     break;
    case 2:
-    ret = reinterpret_cast<JitFnArg2>(info->exec_)(args[0]->IntegerValue(),
+    ret = reinterpret_cast<JitFnArg2>(wrap->exec_)(args[0]->IntegerValue(),
                                                    args[1]->IntegerValue());
     break;
    case 3:
-    ret = reinterpret_cast<JitFnArg3>(info->exec_)(args[0]->IntegerValue(),
+    ret = reinterpret_cast<JitFnArg3>(wrap->exec_)(args[0]->IntegerValue(),
                                                    args[1]->IntegerValue(),
                                                    args[2]->IntegerValue());
     break;
@@ -139,58 +75,17 @@ Handle<Value> ExecInfo::Exec(const Arguments& args) {
 }
 
 
-Handle<Value> ExecInfo::GetAbsoluteOffset(const Arguments& args) {
-  HandleScope scope;
-
-  ExecInfo* info = ObjectWrap::Unwrap<ExecInfo>(args.This());
-
-  if (args.Length() < 1 || !args[0]->IsNumber()) {
-    return ThrowException(Exception::TypeError(String::New(
-        "First argument should be a Number!")));
-  }
-
-  intptr_t addr = reinterpret_cast<intptr_t>(info->exec_);
-  addr += args[0]->IntegerValue();
-
-  return scope.Close(GetPointerBuffer(reinterpret_cast<void*>(addr))->handle_);
-}
-
-
-Handle<Value> ExecInfo::GetPointer(const Arguments& args) {
-  HandleScope scope;
-
-  if (args.Length() < 1 ||
-      !args[0]->IsObject() ||
-      !Buffer::HasInstance(args[0].As<Object>())) {
-    return ThrowException(Exception::TypeError(String::New(
-        "First argument should be Buffer!")));
-  }
-
-  int64_t offset = 0;
-  if (args.Length() == 2)
-    offset = args[1]->IntegerValue();
-
-  char* data = Buffer::Data(args[0].As<Object>()) + offset;
-
-  return scope.Close(GetPointerBuffer(reinterpret_cast<void*>(data))->handle_);
-}
-
-
-void ExecInfo::Init(Handle<Object> target) {
+void FunctionWrap::Init(Handle<Object> target) {
   HandleScope scope;
 
   sym_buffer = Persistent<String>::New(String::NewSymbol("buffer"));
-  kPageSize = sysconf(_SC_PAGE_SIZE);
 
   Local<FunctionTemplate> t = FunctionTemplate::New(New);
   t->InstanceTemplate()->SetInternalFieldCount(1);
 
   NODE_SET_PROTOTYPE_METHOD(t, "exec", Exec);
-  NODE_SET_PROTOTYPE_METHOD(t, "getAbsoluteOffset", GetAbsoluteOffset);
 
-  target->Set(String::NewSymbol("ExecInfo"), t->GetFunction());
-
-  NODE_SET_METHOD(target, "getPointer", GetPointer);
+  target->Set(String::NewSymbol("FunctionWrap"), t->GetFunction());
 }
 
 
@@ -274,7 +169,7 @@ void Runtime::Init(Handle<Object> target) {
 
 
 static void Init(Handle<Object> target) {
-  ExecInfo::Init(target);
+  FunctionWrap::Init(target);
   Runtime::Init(target);
 }
 
--- src/jit.h
+++ src/jit.h
@@ -3,10 +3,10 @@
 
 namespace jit {
 
-class ExecInfo : public node::ObjectWrap {
+class FunctionWrap : public node::ObjectWrap {
  public:
-  ExecInfo(void* exec, size_t elen, void* guard, size_t glen);
-  ~ExecInfo();
+  FunctionWrap(void* exec) : exec_(exec) {
+  }
 
   static void Init(v8::Handle<v8::Object> target);
 
@@ -15,13 +15,8 @@ class ExecInfo : public node::ObjectWrap {
 
   static v8::Handle<v8::Value> New(const v8::Arguments& args);
   static v8::Handle<v8::Value> Exec(const v8::Arguments& args);
-  static v8::Handle<v8::Value> GetAbsoluteOffset(const v8::Arguments& args);
-  static v8::Handle<v8::Value> GetPointer(const v8::Arguments& args);
 
   void* exec_;
-  void* guard_;
-  size_t elen_;
-  size_t glen_;
 };
 
 class Runtime : public node::ObjectWrap {
