2e07125e7bce220ee5547cc46d418deaaf16ab6e
Fedor Indutny <fedor@indutny.com>
2015-09-24 16:17:14 -0400

    x64: 32bit floating-point instructions

--- test/x64/fp-test.js
+++ test/x64/fp-test.js
@@ -5,7 +5,7 @@ if (process.arch !== 'x64')
   return;
 
 describe('JIT.js x64 Floating Point', function() {
-  test('should support fp', function() {
+  test('should support double-fp', function() {
     this.loadDouble('rax', 13589.13589);
     this.movq('xmm1', 'rax');
     this.movq('r8', 'xmm1');
@@ -17,4 +17,17 @@ describe('JIT.js x64 Floating Point', function() {
 
     this.Return();
   }, 184664615);
+
+  test('should support float-fp', function() {
+    this.loadFloat('rax', 13589.13589);
+    this.movd('xmm1', 'rax');
+    this.movd('r8', 'xmm1');
+    this.movd('xmm15', 'r8');
+    this.mulss('xmm1', 'xmm15');
+    this.roundss('up', 'xmm1', 'xmm1');
+    this.cvtss2si('r8', 'xmm1');
+    this.xchg('r8', 'rax');
+
+    this.Return();
+  }, 184664608);
 });
